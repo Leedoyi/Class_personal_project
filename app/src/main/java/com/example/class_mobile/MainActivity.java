8b43e6110563bb27820511a3200c370c63f0dca7
jsmme <doyi9917@naver.com>
2022-10-25 10:04:42 +0900

    First commmit test

---
+++ app/src/main/java/com/example/class_mobile/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.class_mobile;
+
+//import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/class_mobile/ProductActivity.java
@@ -0,0 +1,7 @@
+package com.example.class_mobile;
+
+import android.app.Activity;
+
+public class ProductActivity extends Activity {
+
+}
